441b7c94609713f391056a9f8e37ec0f73fd6f49
Celina Jackson <celinanjackson@gmail.com>
2020-08-24 00:06:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,10 +53,8 @@ attr_reader :id
   end
   
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-
-    result = DB[:conn].execute(sql, name)[0]
-    self.new_from_db(result)
+   sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
   
   def update
